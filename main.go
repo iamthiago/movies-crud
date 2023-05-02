271d14692a92a8a58e93e1dca1d5383b91530289
thiago.pereira <thiagoandrade6@gmail.com>
2023-05-02 17:05:04 +0200

    Add Update Movie
    Update movies by sending a PUT request

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
@@ -69,5 +70,22 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = rand.Int63n(1000000)
 	moviesMap[movie.ID] = movie
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.ParseInt(params["id"], 10, 64)
+
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = id
+	moviesMap[id] = movie
 	json.NewEncoder(w).Encode(movie)
 }

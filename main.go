4ae3d9481aaefd73b925a88097ace1cded57506c
thiago.pereira <thiagoandrade6@gmail.com>
2023-05-02 17:22:02 +0200

    Add Delete endpoint
    Delete movies from the in memory map

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ type Director struct {
 }
 
 var moviesMap = make(map[int64]Movie)
-var movies = make([]Movie, 0, len(moviesMap))
 
 func main() {
 	r := mux.NewRouter()
@@ -33,14 +32,11 @@ func main() {
 	moviesMap[2] = Movie{ID: 2, Isbn: "456", Title: "Movie 2", Director: &Director{Firstname: "Alex", LastName: "Smith"}}
 	moviesMap[3] = Movie{ID: 3, Isbn: "789", Title: "Movie 3", Director: &Director{Firstname: "Jane", LastName: "Black"}}
 
-	for _, m := range moviesMap {
-		movies = append(movies, m)
-	}
-
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
@@ -48,6 +44,12 @@ func main() {
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var movies = make([]Movie, 0, len(moviesMap))
+
+	for _, m := range moviesMap {
+		movies = append(movies, m)
+	}
+
 	json.NewEncoder(w).Encode(movies)
 }
 
@@ -88,4 +90,19 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	movie.ID = id
 	moviesMap[id] = movie
 	json.NewEncoder(w).Encode(movie)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.ParseInt(params["id"], 10, 64)
+
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	delete(moviesMap, id)
+
+	w.WriteHeader(http.StatusOK)
 }
